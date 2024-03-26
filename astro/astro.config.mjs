dec6046e2c845f16789cf1be2d624858fec8fe0e
Joel Franco <joelweet@gmail.com>
2024-03-26 17:05:37 +0100

    first commit

---
+++ astro/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+  integrations: [tailwind()]
+});
---
+++ astro/src/config/index.js
@@ -0,0 +1,7 @@
+const IP = "159.223.31.62";
+
+export default {
+    API_URL: `https://ricardhernandez.com/api`,
+    IMG_URL: `https://ricardhernandez.com/img`,
+
+}
